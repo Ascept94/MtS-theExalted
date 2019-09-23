7262d51b7a480c4d2391d28e9d63aa41baf666ae
Ascept94 <55703303+Ascept94@users.noreply.github.com>
2019-09-23 18:45:21 +0200

    Add files via upload

---
+++ characters/TheExalted.java
@@ -0,0 +1,313 @@
+package Bromod.characters;
+
+import Bromod.relics.Ordis;
+import basemod.abstracts.CustomPlayer;
+import basemod.animations.SpriterAnimation;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.MathUtils;
+import com.esotericsoftware.spine.AnimationState;
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.EnergyManager;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.helpers.ScreenShake;
+import com.megacrit.cardcrawl.localization.CharacterStrings;
+import com.megacrit.cardcrawl.screens.CharSelectInfo;
+import com.megacrit.cardcrawl.unlock.UnlockTracker;
+import com.sun.org.apache.xpath.internal.operations.Or;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import Bromod.DefaultMod;
+import Bromod.cards.*;
+import Bromod.relics.DefaultClickableRelic;
+import Bromod.relics.PlaceholderRelic;
+import Bromod.relics.PlaceholderRelic2;
+
+import java.util.ArrayList;
+
+import static Bromod.DefaultMod.*;
+
+//Wiki-page https://github.com/daviscook477/BaseMod/wiki/Custom-Characters
+//and https://github.com/daviscook477/BaseMod/wiki/Migrating-to-5.0
+//All text (starting description and loadout, anything labeled TEXT[]) can be found in DefaultMod-character-Strings.json in the resources
+
+public class TheExalted extends CustomPlayer {
+    public static final Logger logger = LogManager.getLogger(DefaultMod.class.getName());
+
+    // =============== CHARACTER ENUMERATORS =================
+    // These are enums for your Characters color (both general color and for the card library) as well as
+    // an enum for the name of the player class - IRONCLAD, THE_SILENT, DEFECT, YOUR_CLASS ...
+    // These are all necessary for creating a character. If you want to find out where and how exactly they are used
+    // in the basegame (for fun and education) Ctrl+click on the PlayerClass, CardColor and/or LibraryType below and go down the
+    // Ctrl+click rabbit hole
+
+    public static class Enums {
+        @SpireEnum
+        public static AbstractPlayer.PlayerClass THE_EXALTED;
+
+        @SpireEnum(name = "BRO_COLOR")
+        public static AbstractCard.CardColor COLOR_BRO;
+        @SpireEnum(name = "BRO_COLOR")
+        public static CardLibrary.LibraryType LIBRARY_COLOR_BRO;
+
+        @SpireEnum(name = "MOD_RARE")
+        public static AbstractCard.CardColor MOD_COLOR_RARE;
+        @SpireEnum(name = "MOD_RARE")
+        public static CardLibrary.LibraryType LIBRARY_MOD_COLOR_RARE;
+
+        @SpireEnum(name = "MOD_UNCOMMON")
+        public static AbstractCard.CardColor MOD_COLOR_UNCOMMON;
+        @SpireEnum(name = "MOD_UNCOMMON")
+        public static CardLibrary.LibraryType LIBRARY_MOD_COLOR_UNCOMMON;
+
+        @SpireEnum(name = "MOD_COMMON")
+        public static AbstractCard.CardColor MOD_COLOR_COMMON;
+        @SpireEnum(name = "MOD_COMMON")
+        public static CardLibrary.LibraryType LIBRARY_MOD_COLOR_COMMON;
+
+
+    }
+
+    // =============== CHARACTER ENUMERATORS  =================
+
+
+    // =============== BASE STATS =================
+
+    public static final int ENERGY_PER_TURN = 3;
+    public static final int STARTING_HP = 100;
+    public static final int MAX_HP = 100;
+    public static final int STARTING_GOLD = 99;
+    public static final int CARD_DRAW = 5;
+    public static final int ORB_SLOTS = 0;
+
+    // =============== /BASE STATS/ =================
+
+
+    // =============== STRINGS =================
+
+    private static final String ID = makeID("ExaltedCharacter");
+    private static final CharacterStrings characterStrings = CardCrawlGame.languagePack.getCharacterString(ID);
+    private static final String[] NAMES = characterStrings.NAMES;
+    private static final String[] TEXT = characterStrings.TEXT;
+
+    // =============== /STRINGS/ =================
+
+
+    // =============== TEXTURES OF BIG ENERGY ORB ===============
+
+    public static final String[] orbTextures = {
+            "BromodResources/images/char/Excalibro/orb/Nothing.png",
+            "BromodResources/images/char/Excalibro/orb/Ring.png",
+            "BromodResources/images/char/Excalibro/orb/Ring2.png",
+            "BromodResources/images/char/Excalibro/orb/Nothing.png",
+            "BromodResources/images/char/Excalibro/orb/Nothing.png",
+            "BromodResources/images/char/Excalibro/orb/Base.png",
+            "BromodResources/images/char/Excalibro/orb/Nothing.png",
+            "BromodResources/images/char/Excalibro/orb/Ring_d.png",
+            "BromodResources/images/char/Excalibro/orb/Ring2_d.png",
+            "BromodResources/images/char/Excalibro/orb/Nothing.png",
+            "BromodResources/images/char/Excalibro/orb/Nothing.png",};
+
+    // =============== /TEXTURES OF BIG ENERGY ORB/ ===============
+
+    // =============== CHARACTER CLASS START =================
+
+    public TheExalted(String name, PlayerClass setClass) {
+        super(name, setClass, orbTextures,
+                "BromodResources/images/char/Excalibro/orb/Nothing.png", null,
+                new SpriterAnimation(
+                        "BromodResources/images/char/Excalibro/Spriter/Bro_Anim_001.scml"));
+
+
+        // =============== TEXTURES, ENERGY, LOADOUT =================  
+
+        initializeClass(null, // required call to load textures and setup energy/loadout.
+                // I left these in DefaultMod.java (Ctrl+click them to see where they are, Ctrl+hover to see what they read.)
+                THE_EXALTED_SHOULDER_1, // campfire pose
+                THE_EXALTED_SHOULDER_2, // another campfire pose
+                THE_EXALTED_CORPSE, // dead corpse
+                getLoadout(), 20.0F, -10.0F, 220.0F, 290.0F, new EnergyManager(ENERGY_PER_TURN)); // energy manager
+
+        // =============== /TEXTURES, ENERGY, LOADOUT/ =================
+
+
+        // =============== ANIMATIONS =================  
+
+        loadAnimation(
+                THE_EXALTED_SKELETON_ATLAS,
+                THE_EXALTED_SKELETON_JSON,
+                1.0f);
+        AnimationState.TrackEntry e = state.setAnimation(0, "animation", true);
+        e.setTime(e.getEndTime() * MathUtils.random());
+
+        // =============== /ANIMATIONS/ =================
+
+
+        // =============== TEXT BUBBLE LOCATION =================
+
+        dialogX = (drawX + 0.0F * Settings.scale); // set location for text bubbles
+        dialogY = (drawY + 220.0F * Settings.scale); // you can just copy these values
+
+        // =============== /TEXT BUBBLE LOCATION/ =================
+
+    }
+
+    // =============== /CHARACTER CLASS END/ =================
+
+    // Starting description and loadout
+    @Override
+    public CharSelectInfo getLoadout() {
+        return new CharSelectInfo(NAMES[0], TEXT[0],
+                STARTING_HP, MAX_HP, ORB_SLOTS, STARTING_GOLD, CARD_DRAW, this, getStartingRelics(),
+                getStartingDeck(), false);
+    }
+
+    // Starting Deck
+    @Override
+    public ArrayList<String> getStartingDeck() {
+        ArrayList<String> retVal = new ArrayList<>();
+
+        logger.info("Begin loading starter Deck Strings");
+
+        retVal.add(Strike_Bro.ID);
+        retVal.add(Strike_Bro.ID);
+        retVal.add(Strike_Bro.ID);
+
+        retVal.add(Defend_Bro.ID);
+        retVal.add(Defend_Bro.ID);
+        retVal.add(Defend_Bro.ID);
+        retVal.add(Defend_Bro.ID);
+
+        retVal.add(SlashDash.ID);
+        retVal.add(SlashDash.ID);
+        retVal.add(SlashDash.ID);
+
+
+        return retVal;
+    }
+
+    // Starting Relics	
+    public ArrayList<String> getStartingRelics() {
+        ArrayList<String> retVal = new ArrayList<>();
+
+        retVal.add(Ordis.ID);
+
+        return retVal;
+    }
+
+    // character Select screen effect
+    @Override
+    public void doCharSelectScreenSelectEffect() {
+        CardCrawlGame.sound.playA("ATTACK_DAGGER_3", 0.25f); // Sound Effect
+        CardCrawlGame.sound.playA("ATTACK_SLASH_1", 1.25f);
+        CardCrawlGame.sound.playA("ORB_LIGHTNING_EVOKE", 1.25f);
+        CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.HIGH, ScreenShake.ShakeDur.SHORT,
+                false); // Screen Effect
+    }
+
+    // character Select on-button-press sound effect
+    @Override
+    public String getCustomModeCharacterButtonSoundKey() {
+        return "ATTACK_DAGGER_3";
+    }
+
+    // Should return how much HP your maximum HP reduces by when starting a run at
+    // Ascension 14 or higher. (ironclad loses 5, defect and silent lose 4 hp respectively)
+    @Override
+    public int getAscensionMaxHPLoss() {
+        return 5;
+    }
+
+    // Should return the card color enum to be associated with your character.
+    @Override
+    public AbstractCard.CardColor getCardColor() {
+        return Enums.COLOR_BRO;
+    }
+
+
+
+    // Should return a color object to be used to color the trail of moving cards
+    @Override
+    public Color getCardTrailColor() {
+        return DefaultMod.COLOR_BRO;
+    }
+
+    // Should return a BitmapFont object that you can use to customize how your
+    // energy is displayed from within the energy orb.
+    @Override
+    public BitmapFont getEnergyNumFont() {
+        return FontHelper.energyNumFontRed;
+    }
+
+    // Should return class name as it appears in run history screen.
+    @Override
+    public String getLocalizedCharacterName() {
+        return NAMES[0];
+    }
+
+    //Which card should be obtainable from the Match and Keep event?
+    @Override
+    public AbstractCard getStartCardForEvent() {
+        return new Strike_Bro();
+    }
+
+    // The class name as it appears next to your player name in-game
+    @Override
+    public String getTitle(AbstractPlayer.PlayerClass playerClass) {
+        return NAMES[1];
+    }
+
+    // Should return a new instance of your character, sending name as its name parameter.
+    @Override
+    public AbstractPlayer newInstance() {
+        return new TheExalted(name, chosenClass);
+    }
+
+    // Should return a Color object to be used to color the miniature card images in run history.
+    @Override
+    public Color getCardRenderColor() {
+        return DefaultMod.COLOR_BRO;
+    }
+
+    // Should return a Color object to be used as screen tint effect when your
+    // character attacks the heart.
+    @Override
+    public Color getSlashAttackColor() {
+        return Bromod.DefaultMod.COLOR_BRO;
+    }
+
+    // Should return an AttackEffect array of any size greater than 0. These effects
+    // will be played in sequence as your character's finishing combo on the heart.
+    // Attack effects are the same as used in DamageAction and the like.
+    @Override
+    public AbstractGameAction.AttackEffect[] getSpireHeartSlashEffect() {
+        return new AbstractGameAction.AttackEffect[]{
+                AbstractGameAction.AttackEffect.SLASH_HORIZONTAL,
+                AbstractGameAction.AttackEffect.SLASH_VERTICAL,
+                AbstractGameAction.AttackEffect.SLASH_DIAGONAL};
+    }
+
+    // Should return a string containing what text is shown when your character is
+    // about to attack the heart. For example, the defect is "NL You charge your
+    // core to its maximum..."
+    @Override
+    public String getSpireHeartText() {
+        return TEXT[1];
+    }
+
+    // The vampire events refer to the base game characters as "brother", "sister",
+    // and "broken one" respectively.This method should return a String containing
+    // the full text that will be displayed as the first screen of the vampires event.
+    @Override
+    public String getVampireText() {
+        return TEXT[2];
+    }
+
+
+}
